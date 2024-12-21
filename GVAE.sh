9d5dc2c4404d177e5a5561755a8245fbb227c9cd
luciancahil <johndoe@example.com>
2024-12-20 21:20:50 -0800

    Fixed error in calculating dimensions

--- GVAE.sh
+++ GVAE.sh
@@ -12,22 +12,7 @@
 #SBATCH --output=output_GVAE.txt
 #SBATCH --error=output_GVAE.txt
 
-cd $SLURM_SUBMIT_DIR
-echo "Hello!"
-module load conda
-echo "Hello1!"
-conda init bash
-echo "Hello2!"
-module load gcc
-echo "Hello3!"
-module load openmpi
-echo "Hello4!"
-module load git
-echo "Hello5!"
-source ~/.bashrc
-echo "Hello6!"
 conda activate gvae
-echo "Hello6!"
 python main.py
 conda deactivate
 
